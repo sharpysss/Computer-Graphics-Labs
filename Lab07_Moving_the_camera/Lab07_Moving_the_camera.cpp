e3efbc037fbe29d2690968fa4b4286f275225aa2
sharpysss <22466273@stu.mmu.ac.uk>
2025-05-12 13:52:49 +0100

    Update Lab07_Moving_the_camera.cpp

--- Lab07_Moving_the_camera/Lab07_Moving_the_camera.cpp
+++ Lab07_Moving_the_camera/Lab07_Moving_the_camera.cpp
@@ -7,9 +7,12 @@
 #include <common/texture.hpp>
 #include <common/maths.hpp>
 #include <common/camera.hpp>
-
+const double pi = 3.14159265358979323846;
 // Function prototypes
-void keyboardInput(GLFWwindow *window);
+void keyboardInput(GLFWwindow* window);
+
+void mouseInput(GLFWwindow* window);
+
 
 // Create camera object
 Camera camera(glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f));
@@ -19,26 +22,30 @@ struct Object
 {
     glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
     glm::vec3 rotation = glm::vec3(0.0f, 1.0f, 0.0f);
-    glm::vec3 scale    = glm::vec3(1.0f, 1.0f, 1.0f);
+    glm::vec3 scale = glm::vec3(1.0f, 1.0f, 1.0f);
     float angle = 0.0f;
     std::string name;
 };
 
-int main( void )
+// Frame timer
+float previousTime = 0.0f;    // time of previous iteration of the loop
+float deltaTime = 0.0f;    // time elapsed since last iteration of the loop
+
+int main(void)
 {
     // =========================================================================
     // Window creation - you shouldn't need to change this code
     // -------------------------------------------------------------------------
     // Initialise GLFW
-    if( !glfwInit() )
+    if (!glfwInit())
     {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
+        fprintf(stderr, "Failed to initialize GLFW\n");
         getchar();
         return -1;
     }
 
     glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -47,8 +54,8 @@ int main( void )
     // Open a window and create its OpenGL context
     GLFWwindow* window;
     window = glfwCreateWindow(1024, 768, "Lab07 Moving the Camera", NULL, NULL);
-    
-    if( window == NULL ){
+
+    if (window == NULL) {
         fprintf(stderr, "Failed to open GLFW window.\n");
         getchar();
         glfwTerminate();
@@ -67,13 +74,21 @@ int main( void )
     // -------------------------------------------------------------------------
     // End of window creation
     // =========================================================================
-    
+
     // Enable depth test
     glEnable(GL_DEPTH_TEST);
-    
+
+    // Enable back face culling
+    glEnable(GL_CULL_FACE);
+
     // Ensure we can capture keyboard inputs
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-    
+
+    // Capture mouse inputs
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwPollEvents();
+    glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+
     // Define cube object
     // Define vertices
     const float vertices[] = {
@@ -91,34 +106,34 @@ int main( void )
          1.0f, -1.0f,  1.0f,
          1.0f,  1.0f, -1.0f,
          1.0f,  1.0f,  1.0f,
-        // back
-         1.0f, -1.0f, -1.0f,
-        -1.0f, -1.0f, -1.0f,
-        -1.0f,  1.0f, -1.0f,
-         1.0f, -1.0f, -1.0f,
-        -1.0f,  1.0f, -1.0f,
-         1.0f,  1.0f, -1.0f,
-        // left
-        -1.0f, -1.0f, -1.0f,
-        -1.0f, -1.0f,  1.0f,
-        -1.0f,  1.0f,  1.0f,
-        -1.0f, -1.0f, -1.0f,
-        -1.0f,  1.0f,  1.0f,
-        -1.0f,  1.0f, -1.0f,
-        // bottom
-        -1.0f, -1.0f, -1.0f,
-         1.0f, -1.0f, -1.0f,
-         1.0f, -1.0f,  1.0f,
-        -1.0f, -1.0f, -1.0f,
-         1.0f, -1.0f,  1.0f,
-        -1.0f, -1.0f,  1.0f,
-        // top
-        -1.0f,  1.0f,  1.0f,
-         1.0f,  1.0f,  1.0f,
-         1.0f,  1.0f, -1.0f,
-        -1.0f,  1.0f,  1.0f,
-         1.0f,  1.0f, -1.0f,
-        -1.0f,  1.0f, -1.0f,
+         // back
+          1.0f, -1.0f, -1.0f,
+         -1.0f, -1.0f, -1.0f,
+         -1.0f,  1.0f, -1.0f,
+          1.0f, -1.0f, -1.0f,
+         -1.0f,  1.0f, -1.0f,
+          1.0f,  1.0f, -1.0f,
+          // left
+          -1.0f, -1.0f, -1.0f,
+          -1.0f, -1.0f,  1.0f,
+          -1.0f,  1.0f,  1.0f,
+          -1.0f, -1.0f, -1.0f,
+          -1.0f,  1.0f,  1.0f,
+          -1.0f,  1.0f, -1.0f,
+          // bottom
+          -1.0f, -1.0f, -1.0f,
+           1.0f, -1.0f, -1.0f,
+           1.0f, -1.0f,  1.0f,
+          -1.0f, -1.0f, -1.0f,
+           1.0f, -1.0f,  1.0f,
+          -1.0f, -1.0f,  1.0f,
+          // top
+          -1.0f,  1.0f,  1.0f,
+           1.0f,  1.0f,  1.0f,
+           1.0f,  1.0f, -1.0f,
+          -1.0f,  1.0f,  1.0f,
+           1.0f,  1.0f, -1.0f,
+          -1.0f,  1.0f, -1.0f,
     };
 
     // Define texture coordinates
@@ -176,77 +191,83 @@ int main( void )
         24, 25, 26, 27, 28, 29,     // bottom
         30, 31, 32, 33, 34, 35      // top
     };
-    
+
     // Create the Vertex Array Object (VAO)
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-    
+
     // Create Vertex Buffer Object (VBO)
     unsigned int VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    
+
     // Create texture buffer
     unsigned int uvBuffer;
     glGenBuffers(1, &uvBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(uv), uv, GL_STATIC_DRAW);
-    
+
     // Create Element Buffer Object (EBO)
     unsigned int EBO;
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    
+
     // Compile shader program
     unsigned int shaderID;
     shaderID = LoadShaders("vertexShader.glsl", "fragmentShader.glsl");
-    
+
     // Load the textures
     unsigned int texture;
     texture = loadTexture("../assets/crate.jpg");
-    
+
     // Send the texture uniforms to the fragment shader
     glUseProgram(shaderID);
     unsigned int textureID;
     textureID = glGetUniformLocation(shaderID, "texture");
     glUniform1i(textureID, 0);
-    
+
     // Cube positions
     glm::vec3 positions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f),
-        glm::vec3( 2.0f,  5.0f, -10.0f),
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -10.0f),
         glm::vec3(-3.0f, -2.0f, -3.0f),
         glm::vec3(-4.0f, -2.0f, -8.0f),
-        glm::vec3( 2.0f,  2.0f, -6.0f),
+        glm::vec3(2.0f,  2.0f, -6.0f),
         glm::vec3(-4.0f,  3.0f, -8.0f),
-        glm::vec3( 0.0f, -2.0f, -5.0f),
-        glm::vec3( 4.0f,  2.0f, -4.0f),
-        glm::vec3( 2.0f,  0.0f, -2.0f),
+        glm::vec3(0.0f, -2.0f, -5.0f),
+        glm::vec3(4.0f,  2.0f, -4.0f),
+        glm::vec3(2.0f,  0.0f, -2.0f),
         glm::vec3(-1.0f,  1.0f, -2.0f)
     };
-    
+
     // Add cubes to objects vector
     std::vector<Object> objects;
     Object object;
     object.name = "cube";
-    for (unsigned int i = 0 ; i < 10 ; i++)
+    for (unsigned int i = 0; i < 10; i++)
     {
         object.position = positions[i];
         object.rotation = glm::vec3(1.0f, 1.0f, 1.0f);
-        object.scale    = glm::vec3(0.5f, 0.5f, 0.5f);
-        object.angle    = Maths::radians(20.0f * i);
+        object.scale = glm::vec3(0.5f, 0.5f, 0.5f);
+        object.angle = Maths::radians(20.0f * i);
         objects.push_back(object);
     }
-    
+
     // Render loop
     while (!glfwWindowShouldClose(window))
     {
+
+        // Update timer
+        float time = glfwGetTime();
+        deltaTime = time - previousTime;
+        previousTime = time;
         // Get inputs
         keyboardInput(window);
-        
+        mouseInput(window);
+
         // Clear the window
         glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -255,23 +276,23 @@ int main( void )
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-        
+
         // Send the UV buffer to the GPU
         glEnableVertexAttribArray(1);
         glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
         glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-        
+
         // Calculate view and projection matrices
         camera.calculateMatrices();
-        
+
         // Loop through objects and draw each one
         for (int i = 0; i < static_cast<unsigned int>(objects.size()); i++)
         {
             // Calculate the model matrix
             glm::mat4 translate = Maths::translate(objects[i].position);
-            glm::mat4 scale     = Maths::scale(objects[i].scale);
-            glm::mat4 rotate    = Maths::rotate(objects[i].angle, objects[i].rotation);
-            glm::mat4 model     = translate * rotate * scale;
+            glm::mat4 scale = Maths::scale(objects[i].scale);
+            glm::mat4 rotate = Maths::rotate(objects[i].angle, objects[i].rotation);
+            glm::mat4 model = translate * rotate * scale;
 
             // Calculate the MVP matrix
             glm::mat4 MVP = camera.projection * camera.view * model;
@@ -284,29 +305,54 @@ int main( void )
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
             glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
         }
-        
+
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
-        
+
         // Swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+
     // Cleanup
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
     glDeleteBuffers(1, &uvBuffer);
     glDeleteProgram(shaderID);
-    
+
     // Close OpenGL window and terminate GLFW
     glfwTerminate();
     return 0;
 }
 
-void keyboardInput(GLFWwindow *window)
+void keyboardInput(GLFWwindow* window)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.eye += 5.0f * deltaTime * camera.front;
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.eye -= 5.0f * deltaTime * camera.front;
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.eye -= 5.0f * deltaTime * camera.right;
+
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.eye += 5.0f * deltaTime * camera.right;
+    camera.eye.y = 0.0f;
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        camera.eye.y += 1 * sin(pi * 0.5);
+}
+
+void mouseInput(GLFWwindow* window)
+{
+    // Get mouse cursor position and reset to centre
+    double xPos, yPos;
+    glfwGetCursorPos(window, &xPos, &yPos);
+    glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+
+    // Update yaw and pitch angles
+    camera.yaw += 0.0005f * float(xPos - 1024 / 2);
+    camera.pitch += 0.0005f * float(768 / 2 - yPos);
 }
